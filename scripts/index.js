0c45f8d0e5004c80770c673700c8db89e7223679
Serhii Kirilenko <ksmua85@gmail.com>
2020-09-30 16:13:35 +0300

    added mobile menu

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,27 @@
 window.onload = () => {
   console.log('page is fully loaded');
 
-  var phonesBtn = document.querySelector("#phones__btn");
-  var phonesMobile = document.querySelector(".phones__mobile");
+  var phonesButtons = document.querySelectorAll(".phones__btn");
+  
 
-  phonesBtn.addEventListener("click", () => {
-    if (phonesMobile.style.display == "block"){
-      phonesMobile.style.display = "none";
-    } else phonesMobile.style.display = "block";
-  })
-};
+  
+  phonesButtons.forEach( (elem) => {
+    var phonesMobile = elem.parentNode.parentNode.children[1];
+    console.dir(elem.parentNode.parentNode.children[1]);
+    elem.addEventListener("click", () => {
+      if (phonesMobile.style.display == "block") {
+        phonesMobile.style.display = "none";
+      } else phonesMobile.style.display = "block";  
+    });  
+  });
+
+  var menuBar = document.querySelector(".menu-bar");
+  var menuBarBtn = document.querySelector(".menu-bar__btn");
+  var body = document.querySelector("body");
+
+  menuBarBtn.addEventListener("click",  () => {
+    menuBar.classList.toggle("active");
+    menuBarBtn.classList.toggle("active");
+    body.classList.toggle("lock");
+  });
+}

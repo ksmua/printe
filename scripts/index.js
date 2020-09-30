421af6bc00775c76fe79efa11d960173b8729114
Serhii Kirilenko <ksmua85@gmail.com>
2020-10-01 00:24:43 +0300

    added search field behavior and clean a coments

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,59 @@ window.onload = () => {
     menuBarBtn.classList.toggle("active");
     body.classList.toggle("lock");
   });
+
+  var searchField = document.querySelector(".search-field");
+  var searchInput = document.querySelector(".search-input");
+  console.log(searchInput);
+  console.dir(searchInput);
+  searchField.addEventListener("mouseenter", ()=> {
+    console.log('mouseenter');
+    // searchInput.classList.remove("hide");
+    // searchInput.classList.add("show");
+    if (!menuBar.classList.contains("active")){
+      searchInput.classList.remove("hide");
+      searchInput.classList.add("show");
+      // searchField.style.width = '300px';
+      // searchField.style.width = '300px';
+      console.dir(searchInput);
+    }
+
+  });
+
+  searchField.addEventListener("mouseleave", ()=> {
+    console.log("mouseleave");
+    // searchInput.classList.remove("hide");
+    // searchInput.classList.add("show");
+    // if (!document.activeElement == searchInput){
+    console.log(searchInput.matches(':focus'));
+    if (!menuBar.classList.contains("active")) {
+      if (!searchInput.matches(':focus')){
+        searchInput.classList.add("hide");
+        searchInput.classList.remove("show");
+        searchField.style.width = 'auto';
+      }
+    }
+  });
+  document.addEventListener("click", (event)=> {
+    if (!menuBar.classList.contains("active")) {
+      if (event.target !== searchInput){
+        searchInput.classList.add("hide");
+        searchInput.classList.remove("show");
+        searchField.style.width = 'auto';
+      }
+    }
+    // if (menuBar.classList.contains("active")) {
+    //   if (event.target !== menuBar) {
+    //     menuBar.classList.remove("active");
+    //   }
+    // }
+
+    console.log("document click");
+  });
+  searchInput.addEventListener("mouseleave", ()=> {
+    console.log("searchInput mouseleave");
+  });
+  // if (!menuBar.classList.contains("active") )
+  console.log(searchField);
+
 }
